fbbade1512df88574333f78914fc462c0366fcd8
Tim 'mithro' Ansell <mithro@mithis.com>
2014-08-04 10:45:48 +1000

    Fxing stuff for Joel's review.

--- .travis/config.sh
+++ .travis/config.sh
@@ -1,6 +1,11 @@
-#!/bin/false
-# Don't execute this file, source it!
+#!/bin/echo Don't execute this file, source it!
 # vim: set ts=4 sw=4 et sts=4 ai:
+#
+# Copyright (c) 2014, Tim 'mithro' Ansell
+# All rights reserved.
+#
+# Avaliable under MIT license - http://opensource.org/licenses/MIT
+# See ../LICENSE file for full text.
 
 if [ -z "$SCRIPT_DIR" ]; then
     echo "$$SCRIPT_DIR not set!"
