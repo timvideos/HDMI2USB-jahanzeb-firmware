34e06482721aac78dae760578d915c18e67836c5
Tim 'mithro' Ansell <mithro@mithis.com>
2014-12-28 21:02:51 +1030

    Removing trailing space.

--- cypress/hdmi2usb.c
+++ cypress/hdmi2usb.c
@@ -34,33 +34,33 @@ void TD_Init(void)             // Called once at startup
 	SYNCDELAY; PINFLAGSAB   = 0x00;
 	SYNCDELAY; PINFLAGSCD   = 0x00;
 	SYNCDELAY; FIFOPINPOLAR = 0x00;
-	
+
 	// Global settings
 	//SYNCDELAY; REVCTL = 0x03;
 	SYNCDELAY; CPUCS  = ((CPUCS & ~bmCLKSPD) | bmCLKSPD1);  // 48MHz
-	SYNCDELAY; IFCONFIG = 0xE3; //1110 0011 
-	
+	SYNCDELAY; IFCONFIG = 0xE3; //1110 0011
+
 	// EP1OUT & EP1IN
 	SYNCDELAY; EP1OUTCFG = 0x00;
 	SYNCDELAY; EP1INCFG  = 0xA0;
-	
+
 	// VALID DIR TYPE1 TYPE0 SIZE 0 BUF1 BUF0
 	SYNCDELAY; EP2CFG = 0xA2;
 	SYNCDELAY; EP4CFG = 0xE2;
 	SYNCDELAY; EP6CFG = 0xDA;//1101 1010
 	SYNCDELAY; EP8CFG = 0x00;
-	
+
 	// 0 INFM1 OEP1 AUTOOUT AUTOIN ZEROLENIN 0 WORDWIDE
 	SYNCDELAY; EP2FIFOCFG = 0x10;  // Auto
 	SYNCDELAY; EP4FIFOCFG = 0x0C;
 	SYNCDELAY; EP6FIFOCFG = 0x0C;
 	SYNCDELAY; EP8FIFOCFG = 0x00;
-	
+
 	SYNCDELAY; EP4AUTOINLENH = 0x02;
 	SYNCDELAY; EP4AUTOINLENL = 0x00;
 	SYNCDELAY; EP6AUTOINLENH = 0x04;
 	SYNCDELAY; EP6AUTOINLENL = 0x00;
-	
+
 	SYNCDELAY; REVCTL = 0x03; // REVCTL.0 and REVCTL.1 set to 1
 	SYNCDELAY; FIFORESET = 0x80; // Reset the FIFO
 	SYNCDELAY; FIFORESET = 0x82;
@@ -77,8 +77,8 @@ void TD_Poll(void)             // Called repeatedly while the device is idle
 
 if (!(EP1INCS & 0x02))      // check if EP1IN is available
   {
-	EP1INBUF[0] = 0x0A;       // if it is available, then fill the first 10 bytes of the buffer with 
-	EP1INBUF[1] = 0x20;       // appropriate data. 
+	EP1INBUF[0] = 0x0A;       // if it is available, then fill the first 10 bytes of the buffer with
+	EP1INBUF[1] = 0x20;       // appropriate data.
 	EP1INBUF[2] = 0x00;
 	EP1INBUF[3] = 0x00;
 	EP1INBUF[4] = 0x00;
@@ -114,7 +114,7 @@ BOOL DR_GetDescriptor(void)
 }
 
 BOOL DR_SetConfiguration(void)   // Called when a Set Configuration command is received
-{  
+{
 
    Configuration = SETUPDAT[2];
    return(TRUE);            // Handled by user code
@@ -131,50 +131,50 @@ BOOL DR_GetConfiguration(void)   // Called when a Get Configuration command is r
 BOOL DR_SetInterface(void)       // Called when a Set Interface command is received
 {
    AlternateSetting = SETUPDAT[2];
-	
-	
+
+
 	if (AlternateSetting == 1)
-	{	
+	{
 		//while ( !(EP2468STAT & bmEP2EMPTY) );  // Wait while FIFO remains "not empty" (i.e while busy)
 		SYNCDELAY; EP2FIFOCFG = 0x00;          // Disable AUTOOUT
 		SYNCDELAY; FIFORESET = bmNAKALL;       // NAK all OUT packets from host
-		SYNCDELAY; FIFORESET = 2;              // Advance EP2 buffers to CPU domain			
+		SYNCDELAY; FIFORESET = 2;              // Advance EP2 buffers to CPU domain
 
-		SYNCDELAY; 
-		EP2FIFOBUF[0] = 'U';			
+		SYNCDELAY;
+		EP2FIFOBUF[0] = 'U';
 		EP2FIFOBUF[1] = 'F';
-		EP2FIFOBUF[2] = 'U';		
-		EP2FIFOBUF[3] = 'V';	
-		EP2FIFOBUF[4] = 'U';		
-		
-		if (valuesArray[2] == 1) // Formate MJPEG 
+		EP2FIFOBUF[2] = 'U';
+		EP2FIFOBUF[3] = 'V';
+		EP2FIFOBUF[4] = 'U';
+
+		if (valuesArray[2] == 1) // Formate MJPEG
 		{
-			EP2FIFOBUF[5] = 'J';		
-		} else { // Formate RAW 
-			EP2FIFOBUF[5] = 'R';				
+			EP2FIFOBUF[5] = 'J';
+		} else { // Formate RAW
+			EP2FIFOBUF[5] = 'R';
 		}
-		
-		EP2FIFOBUF[6] = 'U';	
-		
+
+		EP2FIFOBUF[6] = 'U';
+
 		if (valuesArray[3] == 1) // Frame DVI // not implemented bz Atlys doesnot support the HPD
 		{
 			EP2FIFOBUF[7] = 'D';
 		} else { // Frame HDMI
-		
+
 			EP2FIFOBUF[7] = 'H';
 		}
-		
-		
+
+
 		// turn on USB
-		EP2FIFOBUF[8] = 'U';		
-		EP2FIFOBUF[9] = 'N';	
-		
+		EP2FIFOBUF[8] = 'U';
+		EP2FIFOBUF[9] = 'N';
+
 		SYNCDELAY; EP2BCH = 0;
 		SYNCDELAY; EP2BCL = 10;
-		
+
 		SYNCDELAY; OUTPKTEND = 0x82;     // Skip uncommitted second packet
 		SYNCDELAY; FIFORESET = 0;              // Release "NAK all"
-		SYNCDELAY; EP2FIFOCFG = 0x10;  // Auto	
+		SYNCDELAY; EP2FIFOCFG = 0x10;  // Auto
 
 		// reset UVC fifo
 		SYNCDELAY; FIFORESET = 0x80;
@@ -223,7 +223,7 @@ BOOL DR_VendorCmnd(void)
 
 void ISR_Sudav(void) interrupt 0
 {
-   
+
    GotSUD = TRUE;            // Set flag
    EZUSB_IRQ_CLEAR();
    USBIRQ = bmSUDAV;         // Clear SUDAV IRQ
@@ -254,7 +254,7 @@ void ISR_Ures(void) interrupt 0
       pConfigDscr = pFullSpeedConfigDscr;
       pOtherConfigDscr = pHighSpeedConfigDscr;
    }
-   
+
    EZUSB_IRQ_CLEAR();
    USBIRQ = bmURES;         // Clear URES IRQ
 }
@@ -264,7 +264,7 @@ void ISR_Susp(void) interrupt 0
     Sleep = TRUE;
    EZUSB_IRQ_CLEAR();
    USBIRQ = bmSUSP;
-  
+
 }
 
 void ISR_Highspeed(void) interrupt 0
